4ccb9707734df4987814438d4bc0e2e480f7b0f3
atuning120 <90119017+atuning120@users.noreply.github.com>
2024-10-21 20:27:32 -0300

    primeros avances deveras

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	"github.com/gin-gonic/gin"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const url = "mongodb+srv://atuning120:C21387541n@cluster0.ketg2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+var mongoClient *mongo.Client
+
+func init() {
+	if err := connect_to_mongodb(); err != nil {
+		log.Fatal("No se pudo conectar a MongoDB")
+	}
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "anashei",
+		})
+	})
+	r.Run()
+}
+
+func connect_to_mongodb() error {
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(url).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	mongoClient = client
+	return err
 }
